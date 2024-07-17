8b7e39d7397d9f593f965fa02f7340a6b1f27d65
menna <mennakhalood01@gmail.com>
2024-07-17 21:40:07 +0300

    Updated Main file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:islami_app/home/home_screen.dart';
+import 'package:islami_app/providers/app_config_provider.dart';
+import 'package:islami_app/quran/surah_details_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'hadees/hadeth_details_screen.dart';
+import 'my_theme_data.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => AppConfigProvider(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,10 +18,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<AppConfigProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: HomeScreen.routeName,
-      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+        SurahDetailsScreen.routeName: (context) => SurahDetailsScreen(),
+        HadethDetailsScreen.routeName: (context) => HadethDetailsScreen(),
+      },
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: Locale(provider.applanguage),
+      theme: MyThemeData.LightMode,
     );
   }
 }
