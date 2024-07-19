68b92e3aaea0e6fbdedd14b3944c1ec0a6e754bb
menna <mennakhalood01@gmail.com>
2024-07-19 20:51:09 +0300

    Updated settings tab.

--- lib/settings/settings_tab.dart
+++ lib/settings/settings_tab.dart
@@ -3,6 +3,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:islami_app/app_colors.dart';
 import 'package:islami_app/providers/app_config_provider.dart';
 import 'package:islami_app/settings/language_bottom_sheet.dart';
+import 'package:islami_app/settings/theme_bottom_sheet.dart';
 import 'package:provider/provider.dart';
 
 class SettingsTab extends StatefulWidget {
@@ -13,6 +14,7 @@ class SettingsTab extends StatefulWidget {
 class _SettingsTabState extends State<SettingsTab> {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
     var provider = Provider.of<AppConfigProvider>(context);
     return Container(
       margin: EdgeInsets.all(15),
@@ -24,7 +26,7 @@ class _SettingsTabState extends State<SettingsTab> {
             style: Theme.of(context).textTheme.bodyMedium,
           ),
           SizedBox(
-            height: 15,
+            height: screenHeight * 0.02,
           ),
           InkWell(
             onTap: () {
@@ -33,7 +35,9 @@ class _SettingsTabState extends State<SettingsTab> {
             child: Container(
               padding: EdgeInsets.all(15),
               decoration: BoxDecoration(
-                  color: AppColors.primaryLightColor,
+                  color: provider.isDarkMode()
+                      ? AppColors.yellowColor
+                      : AppColors.primaryLightColor,
                   borderRadius: BorderRadius.circular(15)),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -42,6 +46,44 @@ class _SettingsTabState extends State<SettingsTab> {
                     provider.applanguage == 'en'
                         ? AppLocalizations.of(context)!.english
                         : AppLocalizations.of(context)!.arabic,
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  Icon(
+                    Icons.arrow_drop_down,
+                    size: 40,
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: screenHeight * 0.02,
+          ),
+          Text(
+            AppLocalizations.of(context)!.theme,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          SizedBox(
+            height: screenHeight * 0.02,
+          ),
+          InkWell(
+            onTap: () {
+              showThemeBottomSheet();
+            },
+            child: Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                  color: provider.isDarkMode()
+                      ? AppColors.yellowColor
+                      : AppColors.primaryLightColor,
+                  borderRadius: BorderRadius.circular(15)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    provider.isDarkMode()
+                        ? AppLocalizations.of(context)!.dark
+                        : AppLocalizations.of(context)!.light,
                     style: Theme.of(context).textTheme.bodySmall,
                   ),
                   Icon(
@@ -60,5 +102,10 @@ class _SettingsTabState extends State<SettingsTab> {
   void showLanguageBottomSheet() {
     showModalBottomSheet(
         context: context, builder: (context) => LanguageBottomSheet());
+  }
+
+  void showThemeBottomSheet() {
+    showModalBottomSheet(
+        context: context, builder: (context) => ThemeBottomSheet());
   }
 }
